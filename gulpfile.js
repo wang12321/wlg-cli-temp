00a4fb6c2423a0e053bf3b6505bee1103ecdf9cb
licai <1036583615@qq.com>
2022-07-29 10:03:27 +0800

    fix: 注释gulp打包版本自动更新代码

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
 const uglify = require('gulp-uglify');
-var bump = require('gulp-bump');
+// var bump = require('gulp-bump');
 
-gulp.task('js',['bump'], function () {
+gulp.task('js', function () {
     return gulp.src('./lib/*.js') // read all
         .pipe(uglify())
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('bump',function(){
-    return gulp.src('./package.json')
-        .pipe(bump({type:'patch'}))
-        .pipe(gulp.dest('./'));
-});
+// gulp.task('bump',function(){
+//     return gulp.src('./package.json')
+//         .pipe(bump({type:'patch'}))
+//         .pipe(gulp.dest('./'));
+// });
 gulp.task('default', ['js'])
